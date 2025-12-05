eb3311f223663d13a695824b37aa8ff72d2f0513
taniabmguedes@gmail.com <taniabmguedes@gmail.com>
2025-12-05 12:46:29 +0000

    customParseIntPipe

---
+++ src/common/pipes/custom-parse-int-pipe.pipe.ts
@@ -0,0 +1,10 @@
+import { BadRequestException, ParseIntPipe } from '@nestjs/common';
+
+export class CustomParseIntPipe extends ParseIntPipe {
+  constructor() {
+    super({
+      exceptionFactory: () =>
+        new BadRequestException('Param needs to be a number'),
+    });
+  }
+}

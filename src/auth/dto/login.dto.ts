ca4f7198015b18e4c0b48cc7b73745bfe650173f
taniabmguedes@gmail.com <taniabmguedes@gmail.com>
2025-12-05 12:35:14 +0000

    pipe validation

--- src/auth/dto/login.dto.ts
+++ src/auth/dto/login.dto.ts
@@ -1,4 +1,9 @@
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+
 export class LoginDto {
+  @IsEmail({}, { message: 'Invalided email' })
   email: string;
+  @IsString({ message: 'Password must be a string' })
+  @IsNotEmpty({ message: 'Passowod cannot be empty' })
   password: string;
 }
